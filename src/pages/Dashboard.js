dfb8129d6ec507410e95728cac39e29164f59887
Satsu <lumawagjamesbryan@gmail.com>
2022-01-04 15:17:17 +0800

    Change gesture label

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -23,7 +23,7 @@ function Dashboard() {
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-5/6 max-w-9xl mx-auto">
 
             {/* Welcome banner */}
             <WelcomeBanner />
