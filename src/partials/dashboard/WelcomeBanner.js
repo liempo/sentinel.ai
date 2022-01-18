70f5164f77096b2fd7b586e512b0ec782831d776
Satsu <lumawagjamesbryan@gmail.com>
2022-01-18 15:17:06 +0800

    Add dynamic greeting

--- src/partials/dashboard/WelcomeBanner.js
+++ src/partials/dashboard/WelcomeBanner.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function WelcomeBanner() {
+
+  const [time, setTime] = React.useState(new Date())
+
   return (
     <div className="relative bg-indigo-200 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
 
@@ -48,7 +51,7 @@ function WelcomeBanner() {
 
       {/* Content */}
       <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-gray-800 font-bold mb-1">Good afternoon, User! 👋</h1>
+        <h1 className="text-2xl md:text-3xl text-gray-800 font-bold mb-1">{time.getHours()< 12 ? "Good morning, User":"Good afternoon, User!"}👋</h1>
         <p>Here is what’s happening with your house today:</p>
       </div>
 
