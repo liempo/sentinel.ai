7aae886069022adf2444856072fdd675842f4c90
Satsu <lumawagjamesbryan@gmail.com>
2022-01-22 01:42:39 +0800

    Add login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React, { useEffect } from 'react';
-import {
-  Switch,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect, useState } from "react"
+import { Switch, Route, useLocation } from "react-router-dom"
 
-import './css/style.scss';
+import "./css/style.scss"
 
-import { focusHandling } from 'cruip-js-toolkit';
-import './charts/ChartjsConfig';
+import { focusHandling } from "cruip-js-toolkit"
+import "./charts/ChartjsConfig"
 
 // Import pages
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
 
 function App() {
-
-  const startTime = new Date ();
-  const location = useLocation();
+  const startTime = new Date()
+  const location = useLocation()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
+    document.querySelector("html").style.scrollBehavior = "auto"
     window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
-    focusHandling('outline');
-  }, [location.pathname]); // triggered on route change
+    document.querySelector("html").style.scrollBehavior = ""
+    focusHandling("outline")
+  }, [location.pathname]) // triggered on route change
 
   return (
     <>
       <Switch>
         <Route exact path="/">
-          <Login/>
-          {/* <Dashboard starttime={startTime} /> */}
+          {!isLoggedIn ? (
+            <Login
+              onlogin={() => {
+                setIsLoggedIn(true)
+              }}
+            />
+          ) : (
+            <Dashboard starttime={startTime} />
+          )}
         </Route>
       </Switch>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,79 @@
-function Login (){
-    return (<div class="min-h-screen bg-gray-100 flex items-center">
-    <div class="container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300">
-      
-      <div class="py-12 p-10 bg-white rounded-xl">
-        <div class="mb-6">
-          <label class="mr-4 text-gray-700 font-bold inline-block mb-2" for="name">Email address</label>
-          <input type="email" class="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded" placeholder="@email" />
+import { useState, useEffect } from "react"
+import UserAvatar from "../images/user-avatar-32.png"
+
+function Login(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  useEffect(() => {
+    if (isLoggedIn) {
+      props.onlogin()
+    }
+  })
+  return (
+    <div class="min-h-screen bg-gray-100 flex items-center">
+      <div class="container mx-auto max-w-md shadow-md hover:shadow-lg transition duration-300">
+        <div class="py-12 p-10 bg-white rounded-xl">
+          <img
+            className="w-32 h-32 m-auto  rounded-full"
+            src={UserAvatar}
+            width="32"
+            height="32"
+            alt="User"
+          />
+
+          <div class="mb-6">
+            <label
+              class="mr-4 text-lg font-semibold text-gray-700 font-bold inline-block mb-2"
+              for="email"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              class="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+              placeholder="@email"
+              onChange={(event) => {
+                setEmail(event.target.value)
+              }}
+            />
+          </div>
+          <div class="">
+            <label
+              class="mr-4 text-lg font-semibold text-gray-700 font-bold inline-block mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              class="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded"
+              placeholder="Password"
+              onChange={(event) => {
+                setPassword(event.target.value)
+              }}
+            />
+          </div>
+          <span class="text-sm text-gray-700 inline-block mt-4 hover:text-indigo-600 hover:underline hover:cursor-pointer transition duration-200">
+            Forget password
+          </span>
+          <button
+            class="w-full mt-6 text-indigo-50 text-lg font-semibold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300"
+            onClick={() => {
+              if (email == "admin" && password == "admin") {
+                setIsLoggedIn(true)
+              } else {
+                alert("Tanginamo")
+                setIsLoggedIn(false)
+              }
+            }}
+          >
+            LOGIN
+          </button>
         </div>
-        <div class="">
-          <label class="mr-4 text-gray-700 font-bold inline-block mb-2" for="name">Password</label>
-          <input type="password" class="border bg-gray-100 py-2 px-4 w-96 outline-none focus:ring-2 focus:ring-indigo-400 rounded" placeholder="Password" />
-        </div>
-        <span class="text-sm text-gray-700 inline-block mt-4 hover:text-indigo-600 hover:underline hover:cursor-pointer transition duration-200">forget password</span>
-        <button class="w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300">LOGIN</button>
       </div>
     </div>
-  </div>)
+  )
 }
 
 export default Login
