80cfd6f8420415954249f69ab1e8680046e76cd0
Liempo <gonzalesalec@gmail.com>
2022-01-27 01:55:15 +0800

    Add poseNet

--- src/partials/dashboard/CameraCard.js
+++ src/partials/dashboard/CameraCard.js
@@ -1,14 +1,12 @@
 import React from "react"
 
-// Import utilities
-import { tailwindConfig } from "../../utils/Utils"
-
 function CameraCard() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-9 bg-white shadow-lg rounded-sm border border-gray-200">
       <header className="px-5 py-4 border-b border-gray-100">
         <h2 className="text-lg font-semibold text-gray-800">Camera 1: </h2>
       </header>
+      <PoseNet className="h-full" />
     </div>
   )
 }
